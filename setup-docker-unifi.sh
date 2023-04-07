f27e55d86ad5b6cca1293c690a061d5d8cf480fd
rrumfelt-matrix <87191437+rrumfelt-matrix@users.noreply.github.com>
2023-04-07 14:07:21 -0400

    Update setup-docker-unifi.sh

--- setup-docker-unifi.sh
+++ setup-docker-unifi.sh
@@ -1,25 +1,32 @@
-sudo dnf check-update
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $(whoami)
-docker run -d \
-  --name=unifi-controller \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -e MEM_LIMIT=1024 `#optional` \
-  -e MEM_STARTUP=1024 `#optional` \
-  -p 8443:8443 \
-  -p 3478:3478/udp \
-  -p 10001:10001/udp \
-  -p 8080:8080 \
-  -p 1900:1900/udp `#optional` \
-  -p 8843:8843 `#optional` \
-  -p 8880:8880 `#optional` \
-  -p 6789:6789 `#optional` \
-  -p 5514:5514/udp `#optional` \
-  -v /path/to/data:/config \
-  --restart unless-stopped \
-  lscr.io/linuxserver/unifi-controller:latest
+yum update -y
+dnf check-update
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $(whoami)
+echo "version: "2.1"
+services:
+  unifi-controller:
+    image: lscr.io/linuxserver/unifi-controller:latest
+    container_name: unifi-controller
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Etc/UTC
+      - MEM_LIMIT=1024 #optional
+      - MEM_STARTUP=1024 #optional
+    volumes:
+      - /unifi:/config
+    ports:
+      - 8443:8443
+      - 3478:3478/udp
+      - 10001:10001/udp
+      - 8080:8080
+      - 1900:1900/udp #optional
+      - 8843:8843 #optional
+      - 8880:8880 #optional
+      - 6789:6789 #optional
+      - 5514:5514/udp #optional
+    restart: unless-stopped" >> docker-compose.yml
+    docker compose up -d
